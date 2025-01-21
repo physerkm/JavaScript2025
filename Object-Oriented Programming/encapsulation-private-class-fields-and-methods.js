b210cac439dffd053009f4e614c24d29f1caa4eb
physerkm <erkam.sis@outlook.com>
2025-01-21 18:01:49 +0300

    Encapsulation: Private Class Fields and Methods

---
+++ Object-Oriented Programming/encapsulation-private-class-fields-and-methods.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// STATIC version of these 4
+
+class Account {
+  locale = navigator.language;
+  bank = 'Bankist';
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface (API)
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  #approveLoan(val) {
+    // Fake method
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+
+  static test() {
+    console.log('TEST');
+  }
+}
+
+const acc1 = new Account('Lebron', 'USD', 1200000);
+acc1.deposit(78000);
+acc1.withdraw(65000);
+console.log(acc1);
+// acc1.#approveLoan(45000);
+// console.log(acc1.#movements);
+
+Account.test();
