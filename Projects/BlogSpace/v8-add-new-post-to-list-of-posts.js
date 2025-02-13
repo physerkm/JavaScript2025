a9e2a7da57aaaca291e4afdf03007419875797cf
physerkm <erkam.sis@outlook.com>
2025-02-13 21:21:39 +0300

    Add New Post to List of Posts

---
+++ Projects/BlogSpace/v8-add-new-post-to-list-of-posts.js
@@ -0,0 +1,45 @@
+"use strict";
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    const postsArr = data.slice(0, 5);
+    let html = "";
+    for (let post of postsArr) {
+      html += `
+                <h3 class="blah">${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `;
+    }
+    document.getElementById("blog-list").innerHTML = html;
+  });
+
+document.getElementById("new-post").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const postTitle = document.getElementById("post-title").value;
+  const postBody = document.getElementById("post-body").value;
+  const data = {
+    title: postTitle,
+    body: postBody,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then((res) => res.json())
+    .then((post) => {
+      document.getElementById("blog-list").innerHTML = `
+                <h3 class="blah">${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+                ${document.getElementById("blog-list").innerHTML}
+            `;
+    });
+});
