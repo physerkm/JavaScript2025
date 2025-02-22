b291eb73f287c1135e663a6ca74d4e9146c88d1d
physerkm <erkam.sis@outlook.com>
2025-02-22 23:16:13 +0300

    Refactor Card Image Placement

---
+++ Projects/GameOfWar/v6-refactor-card-image-placement.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let deckId;
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardBtn = document.getElementById("draw-cards");
+
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      deckId = data.deck_id;
+    });
+}
+
+newDeckBtn.addEventListener("click", handleClick);
+
+drawCardBtn.addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((response) => response.json())
+    .then((data) => {
+      cardsContainer.children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `;
+      cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `;
+    });
+});
