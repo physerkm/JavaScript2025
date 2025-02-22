4e2e126b6043cb5bddd6a2b2af5701abcaa42f56
physerkm <erkam.sis@outlook.com>
2025-02-23 00:41:32 +0300

    Determine the Winning Card Part 1

---
+++ Projects/GameOfWar/v7-determine-the-winning-card-part1.js
@@ -0,0 +1,68 @@
+"use strict";
+
+let deckId;
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardBtn = document.getElementById("draw-cards");
+
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      deckId = data.deck_id;
+    });
+}
+
+newDeckBtn.addEventListener("click", handleClick);
+
+drawCardBtn.addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      cardsContainer.children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `;
+      cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `;
+    });
+});
+
+function determineCardWinner(card1, card2) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const card1ValueIndex = valueOptions.indexOf(card1.value);
+  const card2ValueIndex = valueOptions.indexOf(card2.value);
+  console.log("card 1:", card1ValueIndex);
+  console.log("card 2:", card2ValueIndex);
+
+  if (card1ValueIndex > card2ValueIndex) {
+    console.log("Card 1 wins!");
+  } else if (card1ValueIndex < card2ValueIndex) {
+    console.log("Card 2 wins!");
+  } else {
+    console.log("It's a tie!");
+  }
+}
+
+const card1Obj = {
+  value: "JACK",
+};
+const card2Obj = {
+  value: "QUEEN",
+};
+
+determineCardWinner(card1Obj, card2Obj);
