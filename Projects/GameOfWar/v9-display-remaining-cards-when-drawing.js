d52dac67544f6f4206bfe5a8eee70532ccf8af7f
physerkm <erkam.sis@outlook.com>
2025-02-26 19:05:31 +0300

    Display Remaining Cards When Drawing

---
+++ Projects/GameOfWar/v9-display-remaining-cards-when-drawing.js
@@ -0,0 +1,64 @@
+"use strict";
+
+let deckId;
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardBtn = document.getElementById("draw-cards");
+const header = document.getElementById("header");
+const remainingText = document.getElementById("remaining");
+
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data)
+      deckId = data.deck_id;
+      console.log(deckId);
+    });
+}
+
+newDeckBtn.addEventListener("click", handleClick);
+
+drawCardBtn.addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      remainingText.textContent = `Remaining cards: ${data.remaining}`;
+      cardsContainer.children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `;
+      cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `;
+      const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
+      header.textContent = winnerText;
+    });
+});
+
+function determineCardWinner(card1, card2) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const card1ValueIndex = valueOptions.indexOf(card1.value);
+  const card2ValueIndex = valueOptions.indexOf(card2.value);
+
+  if (card1ValueIndex > card2ValueIndex) {
+    return "Card 1 wins!";
+  } else if (card1ValueIndex < card2ValueIndex) {
+    return "Card 2 wins!";
+  } else {
+    return "War!";
+  }
+}
