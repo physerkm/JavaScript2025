6495b6a602ae7c0381df37a40173da534dcd0cf1
physerkm <erkam.sis@outlook.com>
2025-03-07 22:24:56 +0300

    Add Author Info

---
+++ Projects/MomentumDashboard/v2-add-author-info.js
@@ -0,0 +1,12 @@
+"use strict";
+
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.full})`;
+    document.getElementById(
+      "author"
+    ).textContent = `Photo by: ${data.user.name}`;
+  });
