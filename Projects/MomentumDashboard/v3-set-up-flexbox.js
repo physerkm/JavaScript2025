db1d21b2c8349cbc51168fd85a1f50e462230f87
physerkm <erkam.sis@outlook.com>
2025-03-08 14:40:38 +0300

    Set up Flexbox

---
+++ Projects/MomentumDashboard/v3-set-up-flexbox.js
@@ -0,0 +1,10 @@
+"use strict";
+
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.full})`;
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
+  });
