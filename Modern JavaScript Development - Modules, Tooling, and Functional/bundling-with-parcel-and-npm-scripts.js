da8a4d1f5f1471ca4b9a3c31987f70fafc324a1f
physerkm <erkam.sis@outlook.com>
2025-03-22 02:55:20 +0300

    Bundling with Parcel and NPM Scripts

---
+++ Modern JavaScript Development - Modules, Tooling, and Functional/bundling-with-parcel-and-npm-scripts.js
@@ -0,0 +1,31 @@
+console.log('Importing module');
+
+import add, { cart } from './shoppingCart2.js';
+add('pencil', 10);
+add('eraser', 25);
+add('notebook', 15);
+
+console.log(cart);
+
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
