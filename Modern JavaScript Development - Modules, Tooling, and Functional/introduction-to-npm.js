d73077970c8be8858168b8cc91e67246a20655b0
physerkm <erkam.sis@outlook.com>
2025-03-29 16:24:31 +0300

    Introduction to NPM

---
+++ Modern JavaScript Development - Modules, Tooling, and Functional/introduction-to-npm.js
@@ -0,0 +1,17 @@
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
