7f5f29af0920250c087ea45a70aec56199fef155
physerkm <erkam.sis@outlook.com>
2025-04-07 22:06:30 +0300

    Callbacks Setup Challenge

---
+++ Asynchronous JavaScript - Promises, Async-Await, and AJAX/callbacks-setup-challenge.js
@@ -0,0 +1,9 @@
+// Challenge: Add a button that, when clicked, gets a new deck of cards from the deckofcards API
+// URL: https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
+// Log the whole response to the console
+
+document.getElementById("new-deck").addEventListener("click", function () {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
