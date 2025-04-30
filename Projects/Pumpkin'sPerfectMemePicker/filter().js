f97ac8e678d52e0a406b3dac52cf8bb47861dd2f
physerkm <erkam.sis@outlook.com>
2025-04-30 22:08:49 +0300

    filter()

---
+++ Projects/Pumpkin'sPerfectMemePicker/filter().js
@@ -0,0 +1,18 @@
+"use strict";
+
+const ages = [1, 5, 9, 23, 56, 10, 47, 70, 10, 19, 23, 18];
+
+const adults = ages.filter(function (age) {
+  return age >= 18;
+});
+console.log(adults);
+
+/*
+Challenge:
+1. Use the .filter method to create an array 
+   called "children" which holds all of the ages 
+   that are under 18.  
+*/
+
+const children = ages.filter((age) => age < 18);
+console.log(children);
