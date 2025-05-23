631ea4d0cdba70a4a2aa68c930574096ab21c59f
physerkm <erkam.sis@outlook.com>
2025-05-23 22:21:38 +0300

    v7 - Remove Duplicates

---
+++ Projects/Pumpkin'sPerfectMemePicker/v7-remove-duplicates.js
@@ -0,0 +1,45 @@
+import { catsData } from "./data2.js";
+
+const emotionRadios = document.getElementById("emotion-radios");
+
+function getEmotionsArray(cats) {
+  const emotionsArray = [];
+
+  for (let cat of cats) {
+    for (let emotion of cat.emotionTags) {
+      /*
+            Challenge:
+            1. Refactor this nested for of so that an 
+               emotion is only pushed to emotionsArray
+               if it is not already in emotionsArray.
+               Extra kudos if you use the "logical not"
+               operator - feel free to google it!
+            */
+
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
+    }
+  }
+  return emotionsArray;
+}
+
+function renderEmotionsRadios(cats) {
+  let radioItems = ``;
+  const emotions = getEmotionsArray(cats);
+  for (let emotion of emotions) {
+    radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`;
+  }
+  emotionRadios.innerHTML = radioItems;
+}
+
+renderEmotionsRadios(catsData);
