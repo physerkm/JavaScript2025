c6561b1ce8f71a5fcd928c4181483b5226a5b9ea
physerkm <erkam.sis@outlook.com>
2025-05-26 23:17:30 +0300

    v9 - Add Colour the Selected Emotion

---
+++ Projects/Pumpkin'sPerfectMemePicker/v9-add-colour-to-the-selected-emotion.js
@@ -0,0 +1,49 @@
+"use strict";
+
+import { catsData } from "./data2.js";
+
+const emotionRadios = document.getElementById("emotion-radios");
+
+emotionRadios.addEventListener("change", highlightCheckedOption);
+
+function highlightCheckedOption(e) {
+  document.getElementById(e.target.id).parentElement.classList.add("highlight");
+  /*
+Challenge:
+1. Change the code in line 8 so we add the 
+   'highlight' class to the parent of the 
+   selected input radio.
+*/
+}
+
+function getEmotionsArray(cats) {
+  const emotionsArray = [];
+  for (let cat of cats) {
+    for (let emotion of cat.emotionTags) {
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
+    }
+  }
+  return emotionsArray;
+}
+
+function renderEmotionsRadios(cats) {
+  let radioItems = ``;
+  const emotions = getEmotionsArray(cats);
+  for (let emotion of emotions) {
+    radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`;
+  }
+  emotionRadios.innerHTML = radioItems;
+}
+
+renderEmotionsRadios(catsData);
