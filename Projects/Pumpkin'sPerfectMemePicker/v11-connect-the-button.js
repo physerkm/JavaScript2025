0c2ef2a437c084941c7bb27ddee09dc869647aaa
physerkm <erkam.sis@outlook.com>
2025-05-29 23:05:07 +0300

    Connect the Button

---
+++ Projects/Pumpkin'sPerfectMemePicker/v11-connect-the-button.js
@@ -0,0 +1,68 @@
+"use strict";
+
+import { catsData } from "./data2.js";
+
+const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
+
+/*
+Challenge:
+1. Set up an eventlistener which calls a new
+   function called "getMatchingCatsArray" when
+   the "Get Image" button is clicked.
+2. getMatchingCatsArray should save the value
+   of the checked radio input to a const and 
+   log out that const.
+*/
+
+emotionRadios.addEventListener("change", highlightCheckedOption);
+
+getImageBtn.addEventListener("click", getMatchingCatsArray);
+
+function highlightCheckedOption(e) {
+  const radios = document.getElementsByClassName("radio");
+  for (let radio of radios) {
+    radio.classList.remove("highlight");
+  }
+  // remove all instances of the highlight class
+  document.getElementById(e.target.id).parentElement.classList.add("highlight");
+}
+
+function getMatchingCatsArray() {
+  const selectedEmotion = document.querySelector(
+    'input[type="radio"]:checked'
+  ).value;
+  console.log(selectedEmotion);
+}
+
+function getEmotionsArray(cats) {
+  const emotionsArray = [];
+  for (let cat of cats) {
+    for (let emotion of cat.emotionTags) {
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
+    }
+  }
+  return emotionsArray;
+}
+
+function renderEmotionsRadios(cats) {
+  let radioItems = ``;
+  const emotions = getEmotionsArray(cats);
+  for (let emotion of emotions) {
+    radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`;
+  }
+  emotionRadios.innerHTML = radioItems;
+}
+
+renderEmotionsRadios(catsData);
